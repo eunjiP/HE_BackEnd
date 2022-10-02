5fafe880205913a6c96cf34ebfd5b83ea0685dc1
eunjiP <dae4227@naver.com>
2022-10-02 23:40:29 +0900

    ..

---
+++ withEarth/app/Views/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://use.fontawesome.com/11b434b08e.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="<?php echo base_url(); ?>assets/css/reset.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/commend.css" rel="stylesheet">
+    <title>행복하지구</title>
+</head>
+<body>
---
+++ withEarth/app/Views/header.php
@@ -0,0 +1,9 @@
+<?php
+    include_once('head.php');
+?>
+<div>
+    <h1>나는 헤더입니다.</h1>
+</div>
+<?php
+    include_once('footer.php');
+?>
