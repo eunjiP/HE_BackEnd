70ac3e2dfb4c597585022814f74e873b6a6610b6
HYE0139 <h.ye011096@gmail.com>
2022-10-03 20:15:02 +0900

    css 경로변경

--- withEarth/app/Views/head.php
+++ withEarth/app/Views/head.php
@@ -7,9 +7,9 @@
     <script src="https://use.fontawesome.com/11b434b08e.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link href="../public/css/reset.css" rel="stylesheet">
-    <link href="../public/css/commend.css" rel="stylesheet">
-    <link href="../public/css/search.css" rel="stylesheet">
+    <link href="/css/reset.css" rel="stylesheet">
+    <link href="/css/commend.css" rel="stylesheet">
+    <link href="/css/search.css" rel="stylesheet">
     <title>행복하지구</title>
 </head>
 <body>
