f82449d7994a7d3fc3d0631a25619f309f83dde9
HYE0139 <h.ye011096@gmail.com>
2022-10-10 21:52:02 +0900

    망할 foreach

--- withEarth/app/Views/searchResult.php
+++ withEarth/app/Views/searchResult.php
@@ -4,28 +4,31 @@
 
 <div class="container">
     <div class="sec-p">
-        <?php if($i_product !== NULL) { ?>
-        <div class="sec-title">'<?= esc($searchName) ?>'의 검색결과 입니다.</div>  
-            <table id="product-list">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>품목명</th>
-                        <th>배출 분류</th>
-                    </tr>
-                </thead>
+        <?php if($allResult['result'] !== null) { ?>
+            <div class="sec-title">'<?= esc($allResult['searchName'])?>' 의 검색결과 입니다.</div>
+                <table id="product-list">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>품목명</th>
+                            <th>배출 분류</th>
+                        </tr>
+                    </thead>
 
-                <tbody>
-                    <tr>
-                        <td class="pd_img"><?= esc($productImg)?></td>
-                        <td><?= esc($productName)?></td>
-                        <td><?= esc($ctnt)?></td>
-                    </tr>
-                </tbody>
-            </table> 
-        </div>
+                    <tbody>
+                        <?php foreach($allResult['result'] as $row) : ?>
+                        <tr>
+                            <td class="pd_img"><?= esc($row['productImg'])?></td>
+                            <td><?= esc($row['productName'])?></td>
+                            <td><?= esc($row['cateName'])?></td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table> 
+            </div>
         <?php } else { ?>
-            <div class="sec-title-null">'<?=esc($searchName)?>'의 검색결과가 없습니다.</div>
+            <div class="sec-title">'<?= esc($allResult['searchName'])?>' 의 검색결과가 없습니다.</div>
+            <span>다른 검색어를 입력해보세요.</span>
         <?php } ?>
     </div>
 </div>
